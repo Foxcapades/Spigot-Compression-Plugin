100d42770ef63dd0713fcc8df2f1ff0615ec13f5
Elizabeth <foxcapade@gmail.com>
2021-12-13 08:17:00 -0500

    stacks can be merged on click of result slot

--- src/main/kotlin/io/foxcapades/spigot/block/compression/event/handler/CustomLeftClickHandler.kt
+++ src/main/kotlin/io/foxcapades/spigot/block/compression/event/handler/CustomLeftClickHandler.kt
@@ -4,6 +4,7 @@ import io.foxcapades.spigot.block.compression.event.BCInvClickEvent
 import io.foxcapades.spigot.block.compression.facades.Facade
 import io.foxcapades.spigot.block.compression.item.ifEmpty
 import io.foxcapades.spigot.block.compression.item.ifNotEmpty
+import org.bukkit.Material.AIR
 import org.bukkit.event.inventory.InventoryType.SlotType.CRAFTING
 import org.bukkit.event.inventory.InventoryType.SlotType.RESULT
 
@@ -37,13 +38,30 @@ internal object CustomLeftClickHandler : ClickHandler {
   private fun BCInvClickEvent.topPut() =
     when (slotType) {
       CRAFTING -> topPutIngredient()
-      RESULT   -> { /* Not permitted, do nothing. */ }
+      RESULT   -> topMergeToCursor()
       else     -> throw IllegalStateException("Called topPut with the invalid slot type: $slotType")
     }
 
   private fun BCInvClickEvent.topPutIngredient() {
-    Facade.logTrace("CustomLeftClickHandler#topPutIngredient()")
     cursor = top.putItem(slotIndex, cursor)
+  }
+
+  private fun BCInvClickEvent.topMergeToCursor() {
+    // If the stacks are incompatible, do nothing.
+    if (inventorySlot.type != cursor.type)
+      return
+
+    val newSize = inventorySlot.amount + cursor.amount
+
+    // If the full slotted stack can't fit in the cursor stack, do nothing.
+    if (newSize > cursor.maxStackSize)
+      return
+
+    // Here we know the slot stack is compatible with the cursor stack and there
+    // is enough room in the cursor stack to hold the entire slotted stack.
+
+    top.popResult()
+    cursor.amount = newSize
   }
 
   private fun BCInvClickEvent.bottomClick() = uncancel()
