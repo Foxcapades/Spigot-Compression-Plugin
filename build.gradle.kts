7256b256fbcb61b814b837d8771b561f9261a121
Elizabeth <foxcapade@gmail.com>
2021-12-21 13:03:39 -0500

    fix config reloading

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group   = "io.foxcapades"
-version = "1.5.3"
+version = "1.6.0"
 
 repositories {
   mavenCentral()
--- src/main/kotlin/io/foxcapades/spigot/block/compression/Config.kt
+++ src/main/kotlin/io/foxcapades/spigot/block/compression/Config.kt
@@ -1,29 +1,73 @@
 package io.foxcapades.spigot.block.compression
 
+import io.foxcapades.spigot.block.compression.facades.Facade
 import io.foxcapades.spigot.block.compression.files.FileManager
 import org.bukkit.Material
-import org.bukkit.configuration.file.FileConfiguration
+import org.yaml.snakeyaml.Yaml
+import java.lang.IllegalStateException
+import java.nio.charset.StandardCharsets
 
 internal object Config {
 
-  private var raw: FileConfiguration = BlockCompressionPlugin.instance!!.config
-
-  object OpenOnInteract {
-    private const val Key = "open-on-interact"
-
-    inline val enabled: Boolean
-      get() = raw["$Key.enabled"] as Boolean
-
-    inline val targetBlockType: Material
-      get() = Material.matchMaterial(raw["$Key.target"] as String)
-        ?: throw IllegalStateException("Invalid $Key.target value.  Must be a valid material id.")
-
-    inline val keyItemType: Material
-      get() = Material.matchMaterial(raw["$Key.holding"] as String)
-        ?: throw IllegalStateException("Invalid $Key.holding value.  Must be a valid material id.")
+  init {
+    reload()
   }
 
+  object OpenOnInteract {
+    internal const val KeyRoot    = "open-on-interact"
+    internal const val KeyEnabled = "enabled"
+    internal const val KeyTarget  = "target"
+    internal const val KeyHolding = "holding"
+
+    var enabled: Boolean = true
+      private set
+
+    var targetBlockType: Material = Material.FLETCHING_TABLE
+      private set
+
+    var keyItemType: Material = Material.DIAMOND
+      private set
+
+    internal fun loadFrom(values: Map<String, Any>) {
+      if (KeyEnabled in values)
+        enabled = values[KeyEnabled] as Boolean
+
+      if (KeyTarget in values) {
+        var id = values[KeyTarget] as String
+
+        if (!id.startsWith("minecraft:"))
+          id = "minecraft:$id"
+
+        targetBlockType = Material.matchMaterial(id)
+          ?: throw IllegalStateException("Invalid open-on-interface.target value: ${values[KeyTarget]}")
+      }
+
+      if (KeyHolding in values) {
+        var id = values[KeyTarget] as String
+
+        if (!id.startsWith("minecraft:"))
+          id = "minecraft:$id"
+
+        keyItemType = Material.matchMaterial(id)
+          ?: throw IllegalStateException("Invalid open-on-interface.holding value: ${values[KeyHolding]}")
+      }
+    }
+  }
+
+  @Suppress("UNCHECKED_CAST")
   fun reload() {
-    raw.load(FileManager.getPluginConfig())
+    Facade.logInfo("Loading plugin config.")
+
+    FileManager.getPluginConfig()
+      .reader(StandardCharsets.UTF_8)
+      .use {
+        Yaml().loadAll(it)
+      }
+      .forEach {
+        val tmp = it as Map<String, Any>
+
+        if (OpenOnInteract.KeyRoot in tmp)
+          OpenOnInteract.loadFrom(tmp)
+      }
   }
 }
