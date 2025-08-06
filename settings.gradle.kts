e282aeb36a262ef1633f4d744bdf4c407c3ca25d
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-08-06 12:49:15 -0400

    add missing toolchain plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 rootProject.name = "spigot-block-compression"
 
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
+}
+
 buildscript {
   repositories {
     mavenCentral()
